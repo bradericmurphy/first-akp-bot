051f0ddfcec43ad529b353f704ff6c4583f77d22
bradericmurphy <bradleyericmurphy@gmail.com>
2018-04-17 10:02:45 -0400

    fixed basic loop, added sleep

--- TestBot0.py
+++ TestBot0.py
@@ -2,8 +2,22 @@ import requests
 from boto.s3.connection import S3Connection
 
 request_params = {'token': os.environ['GROUPME_BOT_ID']}
-while true:
-    response_messages = requests.get('https://api.groupme.com/v3/groups/39940851/messages', params = request_params).json()['response']['messages']
 
-    for message in response_messages['messages']:
-        print message['text']
+while True:
+
+    response = requests.get('https://api.groupme.com/v3/groups/39940851/messages', params = request_params)
+
+    # If there are new messages, check whether any of them are making queries to the bot
+    if (response.status_code == 200):
+        response_messages = response.json()['response']['messages']
+
+        # Iterate through each message, checking its text
+        for message in response_messages:
+            if (message['text']=='hello'):
+                to_send = 'Hello!'
+                post_params = { 'bot_id' : 'GROUPME_BOT_ID', 'text': to_send }
+                requests.post('https://api.groupme.com/v3/bots/post', params = post_params)
+                request_params['since_id'] = message['id']
+                break
+
+    time.sleep(5)
